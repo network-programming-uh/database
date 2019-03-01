1f64b2bd57dd6a9bd9828fb70d9f50a8d9ef2b77
Joel David H. Cruz <jdavidhc1710@gmail.com>
2019-03-01 11:20:49 -0500

    Added .idea/* to .gitignore and jd_tester.py

---
+++ client/test/jd_tester.py
@@ -0,0 +1,64 @@
+import os
+import unittest
+
+from client import Client
+
+SERVER_URL = os.getenv('SERVER_URL')
+
+
+class Simple(unittest.TestCase):
+
+    def setUp(self):
+        self.client = Client(SERVER_URL)
+
+        self.data = [
+            {'_key': 'first', 'data': '1'},
+            {'_key': 'second', 'data': '2'},
+            {'_key': 'third', 'data': '3'}
+        ]
+
+    def test_collections_connection(self):
+        self.client.collection('test_collection')
+        # collection_exits -> boolean
+        self.assertTrue(self.client.collection_exists('test_collection'))  # returns true if collection exits
+        self.client.drop_collection('test_collection')
+        self.assertFalse(self.client.collection_exists('test_collection'))  # return true if collections drop correctly
+
+    def test_data(self, data):
+        for d in data:
+            self.assertEqual(d['_key'], self.client.get(d['_key']))
+            self.assertEqual(d['data'], self.client.get(d['data']))
+
+    # Test for get, replace, update, create, delete operations
+    def test_data_operations(self):
+        # create, get
+        self.client.collection('test_collection')
+        for d in self.data:
+            self.client.create(d)
+        self.test_data(self.data)
+
+        # update
+        self.client.update('first', '3')
+        new_data = [
+            {'_key': 'third', 'data': '1'},
+            {'_key': 'second', 'data': '2'},
+            {'_key': 'first', 'data': '3'}
+        ]
+        self.test_data(new_data)
+
+        # replace
+        self.client.replace('second', '3')
+        new_data = [
+            {'_key': 'third', 'data': '1'},
+            {'_key': 'second', 'data': '3'},
+            {'_key': 'first', 'data': '3'}
+        ]
+        self.test_data(new_data)
+
+        # delete
+        self.client.delete('first')
+        new_data = [
+            {'_key': 'third', 'data': '1'},
+            {'_key': 'second', 'data': '3'}
+        ]
+        self.test_data(new_data)
